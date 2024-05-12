914e34e726224b7d654d51d5a4d3f1b515eabd21
Robert Rumney <robertrumney@gmail.com>
2024-05-12 16:52:10 +0200

    Create GameInitializer.cs

---
+++ GameInitializer.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour
+{
+    private void Start()
+    {
+        string[] args = Environment.GetCommandLineArgs();
+
+        // Look for the '-windowed' argument
+        bool isWindowed = Array.IndexOf(args, "-windowed") >= 0;
+
+        if (isWindowed)
+        {
+            // Set the game to run in windowed mode
+            Screen.fullScreen = false;
+            Screen.SetResolution(1280, 720, false); // You can adjust this resolution
+        }
+        else
+        {
+            // Set the game to run in full-screen mode
+            Screen.fullScreen = true;
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+        }
+    }
+}
